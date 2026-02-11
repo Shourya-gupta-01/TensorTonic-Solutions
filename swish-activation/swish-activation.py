5baf186bc4c86e9748b8e8122af9648aa6243ce6
Shourya Gupta <shouryaagupta0@gmail.com>
2026-02-11 18:46:30 +0530

    Add solution for swish-activation

---
+++ swish-activation/swish-activation.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+def swish(x):
+    """
+    Implement Swish activation function.
+    """
+    # Write code here
+    x = np.asarray(x).astype('float')
+
+    ans = x * (1 / (1 + np.exp(-1 * x)))
+    return ans
